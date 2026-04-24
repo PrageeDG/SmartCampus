229c55a0d085e01fc48c38acfb0ebaaf187ba32b
Dilhara0918 <abeysekaradilhara23@gmail.com>
2026-04-25 00:46:38 +0530

    ResourceRepositoryAnd Api

--- smartcampus-backend/src/main/java/com/sliit/smartcampus/resource/repository/ResourceRepository.java
+++ smartcampus-backend/src/main/java/com/sliit/smartcampus/resource/repository/ResourceRepository.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface ResourceRepository extends MongoRepository<Resource, UUID> {
 
